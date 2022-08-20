833129375151cb0a4b21b17aaf8e9aea9a4965c1
pronepoet <ochiengphillip84@gmail.com>
2022-08-21 02:00:18 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(dir, name){
+  return dir.filter(dir => dir.toLowerCase() === name.toLowerCase())
+}
+function fuzzyMatch(dir, name){
+  return dir.filter(dir => dir.slice(0,2) === name.slice(0,2));
+}
+function matchName (dir, name){
+  return dir.filter(dir => dir.name === name)
+}
